3e58a533732076f9bcb8d4a3756498e9da7bfa13
ArminZheng <2730083511@qq.com>
2020-07-17 02:05:21 +0800

    basis

---
+++ src/main/java/com/az/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.az.controller;
+
+import com.az.dao.EmployeeDao;
+import com.az.entities.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Collection;
+
+@Controller
+public class EmployeeController {
+    @Autowired
+    EmployeeDao employeeDao;
+    //查询所有员工返回
+    @GetMapping("/emps")
+    public String list(Model model){
+        Collection<Employee> all = employeeDao.getAll();
+        model.addAttribute("emps",all);
+        return "emp/list";
+    }
+}
